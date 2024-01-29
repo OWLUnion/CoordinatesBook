f9d8eda53753f9712cd08e289354d77fcbf2e4dc
AlexXuCN <alexxucn0@Outlook.com>
2024-01-29 11:56:18 +0800

    上传了没写完的js和addon的manifest

---
+++ main.js
@@ -0,0 +1,21 @@
+const config = new JsonConfigFile("./plugins/AlexXuCN/CoordinatesBook.json",JSON.stringify({
+    item: "owl:coordinates_book",
+    maxEntries: 50
+}))
+
+function showBook(pl,it) {
+    let data = it.getNbt()
+    data.getKeys().includes("Coordinates") || data.setTag("Coordinates")
+    if (data.getTypeOf("Coordinates") != NBT.List) {
+        pl.sendSimpleForm("数据损坏",
+            "物品数据类型错误：" + data.getTypeOf("Coordinates") + "\n" + data.getTag("Coordinates").toString(2),
+            ["确定"],[],()=>{})
+    }
+    
+}
+
+function wrongData(pl,data) {
+    
+}
+
+log(mc.getOnlinePlayers()[0].getHand().getNbt().getTag("Count").toString(4))
