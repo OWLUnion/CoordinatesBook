d43ccdd43fa812fd3eb3b715d7ee95a25591e192
AlexXuCN <alexxucn0@Outlook.com>
2024-01-29 12:53:59 +0800

    现在可以显示名字了

--- main.js
+++ main.js
@@ -9,8 +9,11 @@ function showBook(pl,it) {
     if (data.getTypeOf("Coordinates") != NBT.List) {
         pl.sendSimpleForm("数据损坏",
             "物品数据类型错误：" + data.getTypeOf("Coordinates") + "\n" + data.getTag("Coordinates").toString(2),
-            ["确定"],[],()=>{})
+            ["确定"],[],()=>{}
+        )
+        return
     }
+    
 }
 
 log(mc.getOnlinePlayers()[0].getHand().getNbt().getTag("Count").toString(4))
